08980e96ce583fd46d36a9fd465820ace188c339
Hieu Nguyen <hieu@email.com>
2021-11-13 14:39:36 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login'
 
 function App() {
   return (
@@ -8,10 +10,14 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route exact path="/home">
+            <Header />
+            <Home />
+          </Route>
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
